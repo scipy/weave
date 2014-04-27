ce57d72b242c42469dc39b577a60fed61121159b
Ralf Gommers <ralf.gommers@googlemail.com>
2014-04-27 15:52:28 +0200

    MAINT: remove weave/doc/, contained outdated version of tutorial.rst

--- weave/setup.py
+++ weave/setup.py
@@ -10,17 +10,5 @@ def configuration(parent_package='',top_path=None):
     config.add_data_dir('tests')
     config.add_data_dir('scxx')
     config.add_data_dir(join('blitz','blitz'))
-    config.add_data_dir('doc')
     config.add_data_dir('examples')
     return config
-
-if __name__ == '__main__':
-    from numpy.distutils.core import setup
-    from .weave_version import weave_version
-    setup(version=weave_version,
-          description="Tools for inlining C/C++ in Python",
-          author="Eric Jones",
-          author_email="eric@enthought.com",
-          licence="SciPy License (BSD Style)",
-          url='http://www.scipy.org',
-          **configuration(top_path='').todict())
